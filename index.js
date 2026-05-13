21be0ab980fb3f39ea971ad820809ab2d1e6b482
PsicoPato1 <nyam15101@gmail.com>
2026-05-12 23:12:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("🌌 CN × NEXOR ONLINE");
+});
+
+client.login(process.env.TOKEN);
